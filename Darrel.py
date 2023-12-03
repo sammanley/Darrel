bd848599ce3b1e577993bbab870385cce077dc60
ImmaFish <120963991+sammanley@users.noreply.github.com>
2023-12-03 08:15:33 -0700

    (Emails + Multiple Domains) Darrel.py
    You can now use Darrel to search for Emails as well as Phone numbers. You can also search multiple sites at once. Very useful for B2B.

--- Darrel.py
+++ Darrel.py
@@ -2,34 +2,79 @@ import re
 import requests
 from urllib.parse import urlparse
 
-# Prompt for the domain URL
-domain_url = input("Enter the domain URL: ")
+def extract_phone_numbers(text):
+    return re.findall(r'\b(\+?1[.-]?)?(\([2-9][0-8][0-9]\)|[2-9][0-8][0-9])[-.]?([0-9]{3})[-.]?([0-9]{4})\b', text)
 
-# Parse the domain URL to extract the domain name
-parsed_url = urlparse(domain_url)
-domain = parsed_url.netloc
+def extract_emails(text):
+    return re.findall(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b', text)
 
-# Check if the domain URL has a schema
-if not parsed_url.scheme:
-    domain_url = "http://" + domain_url
+def print_divider():
+    print("-" * 29)
 
-# Prompt for the filename
-filename = input("Enter the filename: ")
+def save_to_file(file, url, unique_phone_numbers, unique_emails):
+    file.write("-" * 29 + '\n')
+    file.write(f"Site: {url}\n")
+    file.write("-" * 29 + '\n')
 
-# Send a GET request to the specified domain
-response = requests.get(domain_url)
+    if unique_phone_numbers:
+        file.write("Phone Numbers:\n")
+        for number in unique_phone_numbers:
+            file.write(number + '\n')
+        file.write("-" * 29 + '\n')
 
-# Extract phone numbers using regular expressions
-phone_numbers = re.findall(r'\b(\+?1[.-]?)?(\([2-9][0-8][0-9]\)|[2-9][0-8][0-9])[-.]?([0-9]{3})[-.]?([0-9]{4})\b', response.text)
+    if unique_emails:
+        file.write("Emails:\n")
+        for email in unique_emails:
+            file.write(email + '\n')
+        file.write("-" * 29 + '\n')
 
-# Remove redundant phone numbers
-unique_phone_numbers = set(''.join(number) for number in phone_numbers)
+def scrape_and_print_info(url):
+    response = requests.get(url)
+    phone_numbers = extract_phone_numbers(response.text)
+    emails = extract_emails(response.text)
 
-# Save the phone numbers to the specified file
-with open(filename, 'w') as file:
-    for number in unique_phone_numbers:
-        file.write(number + '\n')
+    unique_phone_numbers = set(''.join(number) for number in phone_numbers)
+    unique_emails = set(emails)
 
-print(f"Phone numbers scraped from {domain} have been saved to {filename}")
+    print_divider()
+    print(f"Site: {url}")
+    print_divider()
 
-# Darrel is a Web Scraper created by Samuel Manley AKA ImmaFish
+    if unique_phone_numbers:
+        print("Phone Numbers:")
+        for number in unique_phone_numbers:
+            print(number)
+        print_divider()
+
+    if unique_emails:
+        print("Emails:")
+        for email in unique_emails:
+            print(email)
+        print_divider()
+
+    return unique_phone_numbers, unique_emails
+
+if __name__ == "__main__":
+    # Prompt for multiple URLs
+    urls = input("Enter one or more URLs separated by spaces: ").split()
+
+    # Prompt for the filename
+    filename = input("Enter the filename: ")
+
+    with open(filename, 'w') as file:
+        for url in urls:
+            # Parse the domain URL to extract the domain name
+            parsed_url = urlparse(url)
+            domain = parsed_url.netloc
+
+            # Check if the domain URL has a schema
+            if not parsed_url.scheme:
+                url = "http://" + url
+
+            # Scraping and printing information
+            unique_phone_numbers, unique_emails = scrape_and_print_info(url)
+
+            # Save the phone numbers and emails to the specified file
+            save_to_file(file, url, unique_phone_numbers, unique_emails)
+
+            print(f"Phone numbers and emails scraped from {domain} have been saved to {filename}\n")
